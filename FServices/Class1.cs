e22cc9ceeddb6649d16db6fffaf63112d22f7a7f
Mikhail Hancharou <engeneer2003@mail.ru>
2021-11-28 22:43:13 +0300

    added lab with dll

---
+++ FServices/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using lab10;
+
+namespace FileService
+{
+    public class FileService<T> : IFileService<T> where T : class
+    {
+        public IEnumerable<T> ReadFile(string fileName)
+        {
+            string path = $@"C:\Users\engen\Desktop\dumpster\isp\labs\lab10\{fileName}";
+            JsonSerializer serializer = new JsonSerializer();
+            using (StreamReader fs = File.OpenText(path))
+            {
+                List<Employee> newList = (List<Employee>)serializer.Deserialize(fs, typeof(List<Employee>));
+                Console.WriteLine($"Music of humanity was reborn\nMy congratulations!!!");
+                return (IEnumerable<T>)newList;
+            }
+        }
+
+        public void SaveData(IEnumerable<T> data, string fileName)
+        {
+            string path = $@"C:\Users\engen\Desktop\dumpster\isp\labs\lab10\{fileName}";
+            JsonSerializer serializer = new JsonSerializer();
+            using (StreamWriter fs = File.CreateText(path))
+            {
+                serializer.Serialize(fs, data);
+                Console.WriteLine("Music of humans has been saved to file");
+            }
+        }
+    }
+}
